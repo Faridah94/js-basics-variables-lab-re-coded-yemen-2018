fe5c4b6fc2428dca9e27a59d35b30a5f4a9fd375
Farida <faridah.absi@gmail.com>
2018-08-28 15:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
-let companyName = 'Scuber';
+const companyName = 'Scuber';
 
-const companyName = 'specialCompany';
  
 let mostProfitableNeighborhood = 'Chelsea';
 
